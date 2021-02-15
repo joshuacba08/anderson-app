68ed00bf6a6184185286944789ff13d238e4fce4
Josue Oroya <joshuacba08@gmail.com>
2021-02-14 22:23:14 -0300

    se agrega footer

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 import Home from './pages/Home'
 
 import './App.css';
+import BarNav from "./components/BarNav";
+import Footer from "./components/Footer";
 
 function App() {
   return (
-    <Home />
+    <BrowserRouter>
+      <BarNav />
+
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+      </Switch>
+
+      <Footer />
+    </BrowserRouter>
   )
 }
 
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import AboutMe from '../components/AboutMe';
 import BarNav from '../components/BarNav';
+import Footer from '../components/Footer';
 import Hero from '../components/Hero';
 
 import './styles/Home.css';
@@ -8,7 +9,7 @@ import './styles/Home.css';
 const Home = () => {
     return (
         <>
-            <BarNav />
+            {/* <BarNav /> */}
             <main className="main-container">
                 <Hero />
                 <AboutMe />
